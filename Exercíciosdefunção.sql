7ea1be1070c17a7b6dc29af030cde3d8984eff44
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-10-09 22:26:54 -0300

    Exercício Função String

---
+++ Exercíciosdefunção.sql
@@ -0,0 +1,24 @@
+﻿# Exercicios-de-fun-o
+
+--Função de String
+
+CREATE TABLE nomes (
+    nome VARCHAR(50)
+);
+
+INSERT INTO nomes (nome) VALUES
+    ('Roberta'),
+    ('Roberto'),
+    ('Maria Clara'),
+    ('João');
+
+SELECT UPPER(nome) FROM nomes;
+
+SELECT nome, LENGTH(nome) AS tam_nome FROM nomes;
+
+SELECT
+    CASE
+        WHEN RIGHT(nome, 1) = 'o' THEN CONCAT('Sr. ', nome)
+        ELSE CONCAT('Sra. ', nome)
+    END AS nome_modificado
+FROM nomes;
