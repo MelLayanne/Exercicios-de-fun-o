2b6e59a9755d2422c83be117aae241251d6e5d01
MelLayanne <104033593+MelLayanne@users.noreply.github.com>
2023-10-09 22:29:58 -0300

    Funções númericas

--- Exercíciosdefunção.sql
+++ Exercíciosdefunção.sql
@@ -22,3 +22,22 @@ SELECT
         ELSE CONCAT('Sra. ', nome)
     END AS nome_modificado
 FROM nomes;
+
+--2. Funções Numéricas:
+CREATE TABLE produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos (produto, preco, quantidade) VALUES
+    ('Capinha', 9.997, 7),
+    ('toalha', 49.999, 3),
+    ('sapato', 99.998, 0);
+
+SELECT produto, ROUND(preco, 2) AS preco_novo FROM produtos;
+
+SELECT produto, ABS(quantidade) AS quantidade_abs FROM produtos;
+
+SELECT AVG(preco) AS mediapreco FROM produtos;
+
